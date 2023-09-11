75d38ef59aba9a6b77ec3ec505f4378387b858ab
CristianJaldinPereira <136993907+CristianJaldinPereira@users.noreply.github.com>
2023-09-10 21:55:38 -0400

    Agregar archivos de proyecto.

---
+++ Source/PVZ_USFX_LAB02/Sol.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Sol.h"
+#include "MovimientoAleatorio.h"
+
+// Sets default values
+ASol::ASol()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+
+
+	MeshSol = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Sol Mesh"));
+	
+	//Para que se muevan todos los actores que se añadan al juego 
+	ComponenteMovimiento = CreateDefaultSubobject<UMovimientoAleatorio>(TEXT("Movimiento"));
+
+	RootComponent = MeshSol;
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SolMesh(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Torus.Shape_Torus'"));
+
+	MeshSol->SetStaticMesh(SolMesh.Object);
+}
+
+// Called when the game starts or when spawned
+void ASol::BeginPlay()
+{
+	Super::BeginPlay();	
+	
+}
+
+// Called every frame
+void ASol::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);	
+
+}
+
